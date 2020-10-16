15d8be91da596a04b192b99516fdad714429b3a3
cjwit <chris.witulski@gmail.com>
2020-10-16 18:29:01 -0400

    Move visual callback code to audio, but still commented

--- scripts/Classes/Loop.js
+++ scripts/Classes/Loop.js
@@ -18,38 +18,4 @@ export class Loop {
       rows++;
     }
   }
-
-
-  // visualCallback(name) {
-  //   var name = name.toLowerCase().replace(" ", "-");
-  //   var row = Array.from(this.example.getElementsByClassName(name + "-box"));
-  //   row = row.filter(box => box.classList.contains("filled-box"));
-  //   var numBoxes = row.length;
-
-  //   // determine the active box
-  //   var active = 0;
-  //   for (let i = 0; i < row.length; i++) {
-  //     if (row[i].classList.contains("active-box")) {
-  //       row[i].classList.remove("active-box");
-  //       active = (i + 1) % numBoxes;
-  //       break;
-  //     }
-  //   }
-
-  //   // style the fade animation for the active box
-  //   var activeBox = row[active];
-  //   activeBox.style.backgroundColor = "#2875a1";
-  //   setTimeout(function () { }, 100);
-  //   setTimeout(function () {
-  //     activeBox.animate({
-  //       backgroundColor: "#570E51"
-  //     }, 1000);
-  //   });
-  //   setTimeout(function () {
-  //     activeBox.style.backgroundColor = "#570E51";
-  //   }, 1000);
-
-  //   // increment which box is active for the next iteration
-  //   activeBox.classList.add("active-box");
-  // }
 }
